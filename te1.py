ab174e79fdc26184f674f497d049a561e65abae4
toqaemad9798 <tuqa.23csp17@student.uomosul.edu.iq>
2025-05-06 10:48:23 +0300

    Update te1.py

--- te1.py
+++ te1.py
@@ -5,4 +5,4 @@ import subprocess
 user_input = input("Enter a command to run: ")
 subprocess.call(["python", "make_windrose.py", net, sid])
 
-db_password = "mypassword123"  # ⚠️ كشف بيانات حساسة
+password = os.getenv("EMAIL_PASSWORD")
